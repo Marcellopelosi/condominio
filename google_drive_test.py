80c5753b8b8d569ea30cba8a728745dad2980d2b
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2024-01-19 18:49:05 +0100

    Update google_drive_test.py

--- google_drive_test.py
+++ google_drive_test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-impoort stremlit as st
+import stremlit as st
 
 url = 'https://drive.google.com/file/d/13odc85XSVGIDzgTlwL5aL0ukszKmviQF/view?usp=drive_link'
 path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
