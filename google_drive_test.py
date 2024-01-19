45b2e2ae9a94898ef13177930892192ebb93d7fa
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2024-01-19 18:50:53 +0100

    Update google_drive_test.py

--- google_drive_test.py
+++ google_drive_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-url = 'https://drive.google.com/file/d/13odc85XSVGIDzgTlwL5aL0ukszKmviQF/view?usp=drive_link'
+url = 'https://drive.google.com/file/d/17OBz9_xmWseEwRNFzowQFJ7PwN-QCDNS/view?usp=drive_link'
 path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df = pd.read_csv(path)
 st.write(df)
