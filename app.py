6498ad3c72a8c6db0623b30ea0aa68758b693443
Marcellopelosi <57409544+Marcellopelosi@users.noreply.github.com>
2024-02-02 12:16:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,69 +2,55 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-def caricare_file(nome_file):
-    dati = pd.read_excel(nome_file)
-    return dati
+def load_file(file):
+    data = pd.read_excel(file)
+    return data
 
-def rendiconto(millesimi, registro):
+def balance_sheet(registry):
+    total_payments = registry[registry["versamento/spesa"] == "versamento"][["importo", "categoria"]].groupby("categoria").sum()
+    total_expenses = registry[registry["versamento/spesa"] == "spesa"][["importo", "categoria"]].groupby("categoria").sum()
+    balance = total_payments.join(total_expenses, lsuffix=" versamenti", rsuffix=" spese")
+    balance["saldo"] = balance["importo versamenti"] - balance["importo spese"]
+    return balance
 
-    rendiconto = millesimi.copy()
-    totali_spese_dict = registro[registro["versamento/spesa"]=="spesa"][["importo", "categoria"]].groupby("categoria").sum().to_dict()
+def financial_statement(millesimi, registry):
+    statement = millesimi.copy()
+    total_expenses_dict = registry[registry["versamento/spesa"] == "spesa"][["importo", "categoria"]].groupby("categoria").sum().to_dict()
     
     importo_features = []
     
-    for cat in registro.categoria.unique():
-      totali_millesimi_cat = sum(millesimi[cat])
-      importo_features.append("importo {}".format(cat))
-      rendiconto["importo {}".format(cat)] = millesimi[cat].apply(lambda x: (totali_spese_dict["importo"][cat]/totali_millesimi_cat)*x)
+    for cat in registry.categoria.unique():
+        total_millesimi_cat = sum(millesimi[cat])
+        importo_features.append("importo {}".format(cat))
+        statement["importo {}".format(cat)] = millesimi[cat].apply(lambda x: (total_expenses_dict["importo"][cat]/total_millesimi_cat)*x)
     
-    rendiconto["totale"] = np.sum(rendiconto[importo_features].values, axis = 1)
-    versamenti_per_condomino = registro[registro["versamento/spesa"]=="versamento"][["importo", "nominativi"]].groupby("nominativi").sum().to_dict()["importo"]
-    rendiconto["versamenti effettuati"] = rendiconto["nominativi"].apply(lambda x: versamenti_per_condomino[x])
-    rendiconto["saldo"] = rendiconto["versamenti effettuati"] - rendiconto["totale"] + rendiconto["Saldo Precedente"]
-    return rendiconto
-
-def mostra_bilancio(registro):
-    tot_versamenti = registro[registro["versamento/spesa"] == "versamento"][["importo", "categoria"]].groupby("categoria").sum()
-    tot_spese = registro[registro["versamento/spesa"] == "spesa"][["importo", "categoria"]].groupby("categoria").sum()
-    bilancio = tot_versamenti.join(tot_spese, lsuffix = " versamenti", rsuffix = " spese")
-    bilancio["saldo"] = bilancio["importo versamenti"] - bilancio["importo spese"]
-    return bilancio
+    statement["totale"] = np.sum(statement[importo_features].values, axis=1)
+    payments_per_resident = registry[registry["versamento/spesa"] == "versamento"][["importo", "nominativi"]].groupby("nominativi").sum().to_dict()["importo"]
+    statement["versamenti effettuati"] = statement["nominativi"].apply(lambda x: payments_per_resident[x])
+    statement["saldo"] = statement["versamenti effettuati"] - statement["totale"] + statement["Saldo Precedente"]
+    return statement
 
 def main():
     st.title('Gestione Condominio')
 
-    # Sezione per caricare i file
-    st.header('Caricare i File')
-    file_millesimi = st.file_uploader('Carica file millesimi (xls/xlsx)', type=['xls', 'xlsx'])
-    file_registro = st.file_uploader('Carica file registro (xls/xlsx)', type=['xls', 'xlsx'])
+    st.sidebar.header('Caricare i File')
+    file_millesimi = st.sidebar.file_uploader('Carica file millesimi (xls/xlsx)', type=['xls', 'xlsx'])
+    file_registry = st.sidebar.file_uploader('Carica file registro (xls/xlsx)', type=['xls', 'xlsx'])
 
-    # Verifica se i file sono stati caricati
-    if file_millesimi is not None and file_registro is not None:
-        dati_millesimi = caricare_file(file_millesimi)
-        dati_registro = caricare_file(file_registro)
+    if file_millesimi is not None and file_registry is not None:
+        millesimi_data = load_file(file_millesimi)
+        registry_data = load_file(file_registry)
 
-        # Esegue la funzione "rendiconto"
-        risultato_rendiconto = rendiconto(dati_millesimi, dati_registro)
-
-        # Mostra il risultato come tabella
         st.header('Risultato - Rendiconto')
-        st.write(risultato_rendiconto)
+        result_financial_statement = financial_statement(millesimi_data, registry_data)
+        st.dataframe(result_financial_statement)
 
         st.header('Bilancio per ogni spesa')
-        st.write(mostra_bilancio(dati_registro))
+        result_balance_sheet = balance_sheet(registry_data)
+        st.dataframe(result_balance_sheet)
 
-        scelta_nome = st.selectbox('Seleziona un condomino:', dati_millesimi.nominativi.unique())
-        st.write(dati_registro[dati_registro["nominativi"] == scelta_nome])
-
-        # # Pulsante per scaricare il risultato come file Excel
-        # st.header('Scaricare il Risultato')
-        # st.markdown(get_table_download_link(risultato_rendiconto), unsafe_allow_html=True)
-
-def get_table_download_link(df):
-    # Funzione per creare un link per scaricare un dataframe come file Excel
-    excel_file = df.to_excel("risultato_rendiconto.xlsx", index=False)
-    return f'<a href="data:application/octet-stream;base64,{excel_file}" download="risultato_rendiconto.xlsx">Scarica file Excel</a>'
+        resident_choice = st.selectbox('Seleziona un condomino:', millesimi_data.nominativi.unique())
+        st.write(registry_data[registry_data["nominativi"] == resident_choice])
 
 if __name__ == "__main__":
     main()
